3ab5ec9460ea12fad1c6d6a39e3d3edc5e86c68b
Podze Oleksandr <dasdeg@gmail.com>
2015-04-24 21:19:41 +0300

    added basic CRUD implementation

--- CALab3.py
+++ CALab3.py
@@ -1,11 +1,72 @@
-from flask import Flask
+from flask import Flask, json, render_template, request
+from flask.ext.pymongo import PyMongo
+import pymongo
 
 app = Flask(__name__)
+mongo = PyMongo(app)
+client = pymongo.MongoClient('localhost', 27017)
+db = client['test']
+id_key = 'id'
+name_key = 'name'
+address_key = 'address'
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('layout.html', message='some shitty messsage')
+
+
+@app.route('/api/users')
+def show_all():
+    t = db.users.find()
+    res = []
+    for item in t:
+        print(item[id_key])
+        res.append({
+            id_key: item[id_key],
+            name_key: item[name_key],
+            address_key: item[address_key]
+        })
+
+    print(res)
+    json_str = json.dumps(res)
+    return json_str
+
+
+@app.route('/api/add', methods=['POST'])
+def add():
+    db.users.insert_one({
+        # NEW OBJECT
+        id_key: db.users.count() + 1,
+        name_key: request.form[name_key],
+        address_key: request.form[address_key]
+    })
+
+
+@app.route('/api/remove', methods=['POST'])
+def remove():
+    db.users.delete_one({id_key: request.form[id_key]})
+
+
+@app.route('/api/edit/<int:id>', methods=['GET'])
+def get_detail_info(id):
+    t = db.users.find_one({id_key: id})
+    res = {}
+    for i in [id_key, name_key, address_key]:
+        res[i] = t[i]
+    json_str = json.dumps(res)
+    return json_str
+
+
+@app.route('/api/edit/<int:id>', methods=['POST'])
+def edit(id):
+    new_user = {
+        id_key: id,
+        name_key: request.form[name_key],
+        address_key: request.form[address_key]
+    }
+    db.users.find_one_and_update({id_key: id}, new_user)
+    return json.dumps(new_user)
 
 
 if __name__ == '__main__':
